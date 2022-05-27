bf76eca679d10758a0b9d2ddf03167f641d91241
MerakchiHibaa <ninamimi305@gmail.com>
2022-05-27 21:38:30 +0100

    backend setup

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors")
+const dotenv = require("dotenv")
+const bodyParser = require("body-parser")
+
+
+// firebase admin
+//const firebaseAdminInitializeApp = require("./config/firebase-admin.config")
+
+// Configure dotenv
+dotenv.config({
+    path: ".env"
+})
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.use(express.static('uploads'));
+
+//// Apply middlewares
+// Allow cross-origin
+app.use(cors())
+
+// Parse data as json
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+    res.send("Server is up and running")
+})
+
+
+app.listen(app.get("port"), () => {
+    console.log(`App is served under ${app.get("port")} port`);
+})
